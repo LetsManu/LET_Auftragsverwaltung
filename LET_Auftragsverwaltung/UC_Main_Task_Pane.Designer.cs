f4db6e83ed1a20c3fe6a854a9d7da831c4ba4ffc
Manuel Weißböck <Manuel.WEISSBOECK@htlvb.at>
2018-08-27 12:38:40 +0200

    UC_Main_Task Menu_Strip Update

--- LET_Auftragsverwaltung/UC_Main_Task_Pane.Designer.cs
+++ LET_Auftragsverwaltung/UC_Main_Task_Pane.Designer.cs
@@ -31,6 +31,17 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.aufträgeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.parameterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.neuerEintragToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.neuerEintragToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportÜbersichtToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.excelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pDFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.auftragToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.händlerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.materialToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hilfeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hilfeToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -38,7 +49,10 @@
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.aufträgeToolStripMenuItem,
-            this.parameterToolStripMenuItem});
+            this.parameterToolStripMenuItem,
+            this.neuerEintragToolStripMenuItem,
+            this.hilfeToolStripMenuItem,
+            this.hilfeToolStripMenuItem1});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(448, 24);
@@ -48,8 +62,8 @@
             // aufträgeToolStripMenuItem
             // 
             this.aufträgeToolStripMenuItem.Name = "aufträgeToolStripMenuItem";
-            this.aufträgeToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
-            this.aufträgeToolStripMenuItem.Text = "Aufträge";
+            this.aufträgeToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.aufträgeToolStripMenuItem.Text = "Übersicht";
             // 
             // parameterToolStripMenuItem
             // 
@@ -57,6 +71,82 @@
             this.parameterToolStripMenuItem.Name = "parameterToolStripMenuItem";
             this.parameterToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
             this.parameterToolStripMenuItem.Text = "Parameter";
+            // 
+            // neuerEintragToolStripMenuItem
+            // 
+            this.neuerEintragToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.neuerEintragToolStripMenuItem1,
+            this.exportÜbersichtToolStripMenuItem});
+            this.neuerEintragToolStripMenuItem.Name = "neuerEintragToolStripMenuItem";
+            this.neuerEintragToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.neuerEintragToolStripMenuItem.Text = "Datei";
+            // 
+            // neuerEintragToolStripMenuItem1
+            // 
+            this.neuerEintragToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.auftragToolStripMenuItem,
+            this.händlerToolStripMenuItem,
+            this.materialToolStripMenuItem});
+            this.neuerEintragToolStripMenuItem1.Name = "neuerEintragToolStripMenuItem1";
+            this.neuerEintragToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.neuerEintragToolStripMenuItem1.Text = "Neuer Eintrag";
+            // 
+            // exportÜbersichtToolStripMenuItem
+            // 
+            this.exportÜbersichtToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.excelToolStripMenuItem,
+            this.pDFToolStripMenuItem});
+            this.exportÜbersichtToolStripMenuItem.Name = "exportÜbersichtToolStripMenuItem";
+            this.exportÜbersichtToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exportÜbersichtToolStripMenuItem.Text = "Export Übersicht";
+            // 
+            // excelToolStripMenuItem
+            // 
+            this.excelToolStripMenuItem.Name = "excelToolStripMenuItem";
+            this.excelToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.excelToolStripMenuItem.Text = "Excel";
+            // 
+            // pDFToolStripMenuItem
+            // 
+            this.pDFToolStripMenuItem.Name = "pDFToolStripMenuItem";
+            this.pDFToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.pDFToolStripMenuItem.Text = "PDF";
+            // 
+            // auftragToolStripMenuItem
+            // 
+            this.auftragToolStripMenuItem.Name = "auftragToolStripMenuItem";
+            this.auftragToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.auftragToolStripMenuItem.Text = "Auftrag";
+            // 
+            // händlerToolStripMenuItem
+            // 
+            this.händlerToolStripMenuItem.Name = "händlerToolStripMenuItem";
+            this.händlerToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.händlerToolStripMenuItem.Text = "Händler";
+            // 
+            // materialToolStripMenuItem
+            // 
+            this.materialToolStripMenuItem.Name = "materialToolStripMenuItem";
+            this.materialToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.materialToolStripMenuItem.Text = "Material";
+            // 
+            // infoToolStripMenuItem
+            // 
+            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            this.infoToolStripMenuItem.Text = "Info";
+            // 
+            // hilfeToolStripMenuItem
+            // 
+            this.hilfeToolStripMenuItem.Name = "hilfeToolStripMenuItem";
+            this.hilfeToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.hilfeToolStripMenuItem.Text = "Fenster";
+            // 
+            // hilfeToolStripMenuItem1
+            // 
+            this.hilfeToolStripMenuItem1.Name = "hilfeToolStripMenuItem1";
+            this.hilfeToolStripMenuItem1.Size = new System.Drawing.Size(44, 20);
+            this.hilfeToolStripMenuItem1.Text = "Hilfe";
             // 
             // UC_Main_Task_Pane
             // 
@@ -77,5 +167,16 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem aufträgeToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem parameterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem neuerEintragToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem neuerEintragToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem auftragToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem händlerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem materialToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportÜbersichtToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem excelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pDFToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hilfeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hilfeToolStripMenuItem1;
     }
 }
