c025d7400c93f6ecaba0f6318f35bd95fc4385ab
Chaftalie <felix.lerchner@icloud.com>
2018-08-27 13:23:52 +0200

    Added Menustrip (wanted to Sync)

--- LET_Auftragsverwaltung/UC_Parameter.Designer.cs
+++ LET_Auftragsverwaltung/UC_Parameter.Designer.cs
@@ -28,10 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(858, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // UC_Parameter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "UC_Parameter";
+            this.Size = new System.Drawing.Size(858, 683);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
     }
 }
