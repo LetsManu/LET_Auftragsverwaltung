f92e479145e5b4bd4ae74403bff7767ce964b877
Chaftalie <felix.lerchner@icloud.com>
2018-08-29 09:19:45 +0200

    Added ObjectViewList mit nuget (Name: ObjectListView.Official)

--- LET_Auftragsverwaltung/UC_Overview.Designer.cs
+++ LET_Auftragsverwaltung/UC_Overview.Designer.cs
@@ -29,24 +29,11 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.auftragsDataSet = new LET_Auftragsverwaltung.auftragsDataSet();
             this.auftragsDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.auftragsDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.auftragsDataSetBindingSource)).BeginInit();
             this.SuspendLayout();
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(831, 459);
-            this.dataGridView1.TabIndex = 0;
             // 
             // auftragsDataSet
             // 
@@ -62,10 +49,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.dataGridView1);
             this.Name = "UC_Overview";
             this.Size = new System.Drawing.Size(837, 464);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.auftragsDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.auftragsDataSetBindingSource)).EndInit();
             this.ResumeLayout(false);
@@ -73,8 +58,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.BindingSource auftragsDataSetBindingSource;
         private auftragsDataSet auftragsDataSet;
     }
