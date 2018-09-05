ecb27139a0d12d84daec0fd38729971bd910b4b8
Manuel Weißböck <Manuel.WEISSBOECK@htlvb.at>
2018-09-05 10:31:17 +0200

    Morning Commit

--- LET_Auftragsverwaltung/UC_New_auftrag.cs
+++ LET_Auftragsverwaltung/UC_New_auftrag.cs
@@ -40,7 +40,6 @@ namespace LET_Auftragsverwaltung
             UC_New_auftrag_fill_cbx_tech();
             UC_New_auftrag_fill_cbx_auf();
             UC_New_auftrag_fill_cbx_lief();
-            //UC_New_auftrag_fill_cbx_stoff_lief();
             date_erstell.Value = DateTime.Today;
             date_mont.Value = DateTime.Today.AddDays(28);
         }
@@ -182,27 +181,33 @@ namespace LET_Auftragsverwaltung
 
         private void UC_New_auftrag_fill_cbx_stoff_lief()
         {
-            try
+            if (cbx_new_auf_lief.Items.Count > 0)
             {
-                OdbcConnection connection = Connection;
-                connection.Open();
-                string sql = "SELECT L_ID, Lieferant FROM Lieferant WHERE deaktiviert<>true";
-                OdbcDataAdapter dc = new OdbcDataAdapter(sql, connection);
-                DataTable dtLief = new DataTable();
-                dc.Fill(dtLief);
-                connection.Close();
+                try
+                {
+                    OdbcConnection connection = Connection;
+                    connection.Open();
+                    string sql =
+                        string.Format("SELECT stoff.ST_ID,stoff.`Stoff` FROM stoff INNER JOIN stoff_lieferant ON stoff.ST_ID = stoff_lieferant.ST_ID WHERE stoff_lieferant.L_ID = {0}", cbx_new_auf_lief.SelectedValue);
+                    OdbcDataAdapter dc = new OdbcDataAdapter(sql, connection);
+                    DataTable dtStoff = new DataTable();
+                    dc.Fill(dtStoff);
+                    connection.Close();
 
 
-                cbx_new_auf_lief.DataSource = dtLief;
-                cbx_new_auf_lief.ValueMember = "L_ID";
-                cbx_new_auf_lief.DisplayMember = "Lieferant";
+                    cbx_new_auf_stoff.DataSource = dtStoff;
+                    cbx_new_auf_stoff.ValueMember = "ST_ID";
+                    cbx_new_auf_stoff.DisplayMember = "Stoff";
 
 
-                if (cbx_new_auf_lief.Items.Count > 0) cbx_new_auf_lief.SelectedIndex = 0;
-            }
-            catch (Exception f)
-            {
-                MessageBox.Show("Fehler in der SQL Abfrage(Neue Auftrag: Fill CBX Lief): \n\n" + f.Message, "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    if (cbx_new_auf_stoff.Items.Count > 0) cbx_new_auf_stoff.SelectedIndex = 0;
+                }
+                catch (Exception f)
+                {
+                    MessageBox.Show("Fehler in der SQL Abfrage(Neue Auftrag: Fill CBX Stoff): \n\n" + f.Message,
+                        "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    connection.Close();
+                }
             }
         }
 
@@ -266,6 +271,105 @@ namespace LET_Auftragsverwaltung
 
         }
 
+
+        private void btn_auftag_delete_Click(object sender, EventArgs e)
+        {
+            lbx_auftrag.Items.Remove(lbx_auftrag.Items[lbx_auftrag.SelectedIndex]);
+
+        }
+
+        private void Btn_new_auf_save_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OdbcConnection connection = Connection;
+                connection.Open();
+                string sql = "INSERT INTO auftrags.schatten (schatten.Notitz) VALUES ('')";
+                OdbcCommand cmd = new OdbcCommand(sql, connection);
+                cmd.ExecuteNonQuery();
+                sql = "SELECT * FROM schatten ORDER BY schatten.S_ID DESC LIMIT 1";
+                cmd = new OdbcCommand(sql, connection);
+                OdbcDataReader sqlReader = cmd.ExecuteReader();
+                sqlReader.Read();
+                int schatten_ID = Convert.ToInt32(sqlReader.ToString());
+                sql = string.Format(
+                    "INSERT INTO auftraege a (a.Auftrags_NR, a.Fertigungsstatus a.Projektverantwortlicher, a.Planer_Techniker, a.Erstelldatum, a.Montage_Datum, a.Projektbezeichnung, a.Schatten,  a.Notitz_Kauf, a.Notitz_Tech) VALUES ('{0}', 6, {1}, {2}, '{3}', '{4}', '{5}', '{6}', '{7}')",
+                    txt_auftrag_nr.Text, cbx_verant.SelectedValue, cbx_tech.SelectedValue, date_erstell.Text, date_mont.Text, txt_auf_proj_ken.Text, schatten_ID, txt_info_kauf.Text, txt_info_tech.Text);
+
+                cmd = new OdbcCommand(sql, connection);
+                cmd.ExecuteNonQuery();
+
+                string sql2 = string.Format(
+                    "SELECT a.ID FROM auftraege a WHERE a.Auftrags_Nr = '{0}' AND a.Projektverantwortlicher = {1} AND a.Planer_Techniker = {2} AND a.Erstelldatum = '{3}' AND a.Montage_Datum = '{4]' AND a.Projektbezeichnung = '{5}' AND a.Notitz_Kauf = '{6}' AND a.Notitz_Tech = '{7}'",
+                    txt_auftrag_nr.Text, cbx_verant.SelectedValue, cbx_tech.SelectedValue, date_erstell.Text,
+                    date_mont.Text, txt_auf_proj_ken.Text, txt_info_kauf.Text, txt_info_tech.Text);
+
+                OdbcCommand cmd_read = new OdbcCommand(sql2, connection);
+                sqlReader = cmd_read.ExecuteReader();
+
+                sqlReader.Read();
+
+                int auf_id = sqlReader.GetInt32(0);
+
+                for (int i = 0; i < lbx_auftrag.Items.Count; i++)
+                {
+                    int art_ID = (lbx_auftrag.Items[i] as Object_auf).ID;
+
+
+                    string sql3 = string.Format("INSERT INTO auftraege_auftragsart a (a.ID, a.Art_ID) VALUES ({0}, {1})",
+                        auf_id, art_ID);
+                    OdbcCommand cmd2 = new OdbcCommand(sql3, connection);
+                    cmd2.ExecuteNonQuery();
+                }
+
+
+
+
+                connection.Close();
+            }
+            catch (Exception f)
+            {
+                MessageBox.Show("Fehler in der SQL Abfrage: \n\n" + f.Message, "Fehler", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            finally
+            {
+                MessageBox.Show("Person wurde gespeichert", "Speicherung erfolgreich", MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+
+            }
+        }
+
+        private void cbx_new_auf_lief_SelectedValueChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void cbx_new_auf_lief_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void cbx_new_auf_lief_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            
+
+
+
+        }
+
+        private void cbx_new_auf_lief_Validated(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void cbx_new_auf_lief_DropDownClosed(object sender, EventArgs e)
+        {
+            cbx_new_auf_stoff.Enabled = true;
+            UC_New_auftrag_fill_cbx_stoff_lief();
+        }
+
         public class Object_auf
         {
             public int ID;
@@ -285,15 +389,5 @@ namespace LET_Auftragsverwaltung
 
         }
 
-        private void btn_auftag_delete_Click(object sender, EventArgs e)
-        {
-            lbx_auftrag.Items.Remove(lbx_auftrag.Items[lbx_auftrag.SelectedIndex]);
-
-        }
-
-        private void Btn_new_auf_save_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
