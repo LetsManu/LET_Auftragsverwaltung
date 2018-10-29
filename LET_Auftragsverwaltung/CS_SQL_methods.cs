49afc4236e5493bcddd7ce6a3f655ba464e6f226
BuildTools <unconfigured@null.spigotmc.org>
2018-10-29 14:13:30 +0100

    Added CS_SQL_methods and tried the new Fill_CBX(string) on two methodes in UC_Parameters

---
+++ LET_Auftragsverwaltung/CS_SQL_methods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LET_Auftragsverwaltung
+{
+
+    class CS_SQL_methods
+    {
+
+        static private OdbcConnection Connection => CS_DB.Connection;
+
+        static public DataTable Fill_CBX(string sql)
+        {
+
+            var da = new OdbcDataAdapter(sql, Connection);
+            var dt_out = new DataTable();
+            da.Fill(dt_out);
+            Connection.Close();
+
+            return dt_out;
+        }
+    }
+}
