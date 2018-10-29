83bf520506fc320b22ace846f29c6a632740e029
Chaftalie <felix.lerchner@icloud.com>
2018-10-29 14:43:41 +0100

    Now the motherboard info addition for CS_DB does work properly on all PCs (even Quantum?)

--- LET_Auftragsverwaltung/CS_DB.cs
+++ LET_Auftragsverwaltung/CS_DB.cs
@@ -11,23 +11,25 @@ namespace LET_Auftragsverwaltung
 {
     class CS_DB
     {
-        private static string login_name = "";
-        private static string login_pw = "";
-        private static string server_IP = "";
-        private static string database = "";
-        private static string port = "";
         private static OdbcConnection connection = null;
         private static bool Is_this_mb = "M80-AC016500071" == CS_System_Info.GetMotherBoardID();
 
         private static string Connectionstring => $"Driver={"MySQL ODBC 5.3 Unicode Driver"};Server={Server_IP};Port={Port};Database={Database};User={Login_name};Password={Login_pw};Option=3;";
 
-        public static OdbcConnection Connection => connection ?? (connection = new OdbcConnection(Connectionstring));
+        public static OdbcConnection Connection => connection ?? (connection = new OdbcConnection(Connectionstring));  
+
+        private static string Database => "auftrags";
 
         private static string Login_name => "root";
 
-        private static string Login_pw => "cola0815";
-
-        private static string Database => "auftrags";
+        private static string Login_pw
+        {
+            get
+            {
+                if (Is_this_mb)return "";
+                else return "cola0815";
+            }
+        }
 
         private static string Server_IP
         {
