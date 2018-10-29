c8c0ab57bca22c5f42a2230faa98f8439c690866
Chaftalie <felix.lerchner@icloud.com>
2018-10-29 13:42:40 +0100

    Refactored now its ez to read and stuff

--- LET_Auftragsverwaltung/CS_DB.cs
+++ LET_Auftragsverwaltung/CS_DB.cs
@@ -18,38 +18,18 @@ namespace LET_Auftragsverwaltung
         private static string port = "";
         private static OdbcConnection connection = null;
 
-        private static string Connectionstring
-        {
-            get
-            {
-                return $"Driver={"MySQL ODBC 5.3 Unicode Driver"};Server={Server_IP};Port={Port};Database={Database};User={Login_name};Password={Login_pw};Option=3;";
-            }
-        }
-        
-        public static OdbcConnection Connection
-        {
-            get { return connection ?? (connection = new OdbcConnection(Connectionstring)); }
-        }
+        private static string Connectionstring => $"Driver={"MySQL ODBC 5.3 Unicode Driver"};Server={Server_IP};Port={Port};Database={Database};User={Login_name};Password={Login_pw};Option=3;";
 
-        private static string Login_name
-        {
-            get { return "root"; }
-        }
-        private static string Login_pw
-        {
-            get { return "cola0815"; }
-        }
-        private static string Server_IP
-        {
-            get { return "localhost"; }
-        }
-        private static string Database
-        {
-            get { return "auftrags"; }
-        }
-        private static string Port
-        {
-            get { return "3306"; }
-        }
+        public static OdbcConnection Connection => connection ?? (connection = new OdbcConnection(Connectionstring));
+
+        private static string Login_name => "root";
+
+        private static string Login_pw => "cola0815";
+
+        private static string Server_IP => "localhost";
+
+        private static string Database => "auftrags";
+
+        private static string Port => "3306";
     }
 }
