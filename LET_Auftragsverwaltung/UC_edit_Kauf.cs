d6412f0a6e5255af28cf851fcc5775f3e1ee9b41
LetsManu <manuel.weissboeck@htlvb.at>
2018-11-13 19:51:22 +0100

    Added UC_Kauf_Edit with Layout

---
+++ LET_Auftragsverwaltung/UC_edit_Kauf.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LET_Auftragsverwaltung
+{
+    public partial class UC_edit_Kauf : UserControl
+    {
+        public UC_edit_Kauf()
+        {
+            InitializeComponent();
+        }
+    }
+}
