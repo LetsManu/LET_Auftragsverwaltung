b55522cd1e104d6304b18d249277fcaedcf17181
Chaftalie <felix.lerchner@icloud.com>
2018-11-16 20:42:32 +0100

    Added Start Method for LET_Auftragsverwaltung_outlook_Pipe NamedPipe
    tested is working beautifully

--- LET_Auftragsverwaltung/CS_Named_Pipes.cs
+++ LET_Auftragsverwaltung/CS_Named_Pipes.cs
@@ -11,6 +11,28 @@ namespace LET_Auftragsverwaltung
 {
     static class CS_Named_Pipes
     {
-        
+        public static void Start( )
+        {
+            Task.Factory.StartNew(( ) =>
+            {
+                while (true)
+                {
+                    var server = new NamedPipeServerStream("LET_Auftragsverwaltung_outlook_Pipe");
+                    server.WaitForConnection();
+                    StreamReader reader = new StreamReader(server);
+                    StreamWriter writer = new StreamWriter(server);
+                    while (server.IsConnected)
+                    {
+                        var line = reader.ReadLine();
+                        if (line != null)
+                        {
+                            writer.WriteLine(String.Join("", line.Reverse()));
+                            writer.Flush();
+                        }
+                    }
+                    server.Dispose();
+                }
+            });
+        }
     }
 }
