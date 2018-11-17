a0b84e9bf47a05c5ea0db555e7e004624be3ad83
Chaftalie <felix.lerchner@icloud.com>
2018-11-17 17:03:35 +0100

    Added Timeout Thread so the programm closes if outlook is not running

--- LET_URL_Client/Program.cs
+++ LET_URL_Client/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.IO.Pipes;
 using System.Linq;
+using System.Net.Mime;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace LET_URL_Client
@@ -14,6 +16,9 @@ namespace LET_URL_Client
         {
             //Edit_Auftrag;ID:;6;LET_SPACE;LET_ENDE
 
+            ThreadStart childref = new ThreadStart(Timout);
+            Thread childThread = new Thread(childref);
+            childThread.Start();
             //Client
             var client = new NamedPipeClientStream("LET_Auftragsverwaltung_outlook_Pipe");
             client.Connect();
@@ -32,6 +37,12 @@ namespace LET_URL_Client
                 Console.WriteLine("Outlook oder das LET Auftragsverwaltungs addin ist nicht gestartet. \nWenn dies nicht der Fall ist Outlook neustarten. \nBei Wiederholung Programmierer hinzuholen.\n\n\nDieses Fenster kann geschlossen werden.\nEine Taste drücken um zu Beenden.");
                 Console.ReadKey();
             }
+        }
+
+        static void Timout()
+        {
+            Thread.Sleep(5000);
+            Environment.Exit(1);
         }
     }
 }
