983a2774d78042703ad3f0298bdf3b72c15e659a
Chaftalie <felix.lerchner@icloud.com>
2018-11-17 13:50:30 +0100

    Added code for LET_URL_Client connect to Pipe and transfer the right phrase with ID from commandline argument

--- LET_URL_Client/Program.cs
+++ LET_URL_Client/Program.cs
@@ -12,8 +12,27 @@ namespace LET_URL_Client
     {
         static void Main(string[ ] args)
         {
+            //Edit_Auftrag;ID:;6;LET_SPACE;LET_ENDE
 
+            //Client
+            var client = new NamedPipeClientStream("LET_Auftragsverwaltung_outlook_Pipe");
+            client.Connect();
 
+            if (client.IsConnected)
+            {
+                StreamReader reader = new StreamReader(client);
+                StreamWriter writer = new StreamWriter(client);
+
+                writer.WriteLine("Edit_Auftrag;ID:;" + args[0].Last() + ";LET_SPACE;LET_ENDE");
+
+                writer.Flush();
+                Console.WriteLine(reader.ReadLine());
+            }
+            else
+            {
+                Console.WriteLine("Outlook oder das LET Auftragsverwaltungs addin ist nicht gestartet. \nWenn dies nicht der Fall ist Outlook neustarten. \nBei Wiederholung Programmierer hinzuholen.\n\n\nDieses Fenster kann geschlossen werden.\nEine Taste drücken um zu Beenden.");
+                Console.ReadKey();
+            }
         }
     }
 }
