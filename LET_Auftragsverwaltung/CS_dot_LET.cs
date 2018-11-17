6e1ad0ea3e2ef8e04942feee6989b5fbb126bc46
Chaftalie <felix.lerchner@icloud.com>
2018-11-17 16:14:19 +0100

    Added CS_dot_LET Class

---
+++ LET_Auftragsverwaltung/CS_dot_LET.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LET_Auftragsverwaltung
+{
+    class CS_dot_LET
+    {
+
+    }
+}
