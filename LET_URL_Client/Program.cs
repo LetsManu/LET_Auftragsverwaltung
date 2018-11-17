2ea57a75eb69487a27993d80ac392cb80b3f57f7
Chaftalie <felix.lerchner@icloud.com>
2018-11-17 18:14:47 +0100

    Added tolower for safety reasons

--- LET_URL_Client/Program.cs
+++ LET_URL_Client/Program.cs
@@ -26,7 +26,7 @@ namespace LET_URL_Client
             {
                 StreamReader reader = new StreamReader(client);
                 StreamWriter writer = new StreamWriter(client);
-                writer.WriteLine("Edit_Auftrag;ID:;" + args[0].Replace("leturl:ID-","") + ";LET_SPACE;LET_ENDE");
+                writer.WriteLine("Edit_Auftrag;ID:;" + args[0].ToLower().Replace("leturl:id-","") + ";LET_SPACE;LET_ENDE");
                 writer.Flush();
                 Console.WriteLine(reader.ReadLine());
             }
