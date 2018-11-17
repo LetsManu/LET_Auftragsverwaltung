86552798b85bb8ddc47685afea09d2bc180224f2
Chaftalie <felix.lerchner@icloud.com>
2018-11-17 13:49:29 +0100

    Added LET_URL_Client Project

---
+++ LET_URL_Client/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LET_URL_Client
+{
+    class Program
+    {
+        static void Main(string[ ] args)
+        {
+
+
+        }
+    }
+}
