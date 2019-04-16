d009a866484e0fc392934b1c5ab81ab03c9fab71
chaftalie <chaftalie@DESKTOP-IHNFJ9N>
2019-04-16 11:34:54 +0200

    Added, so Parameter stuff is located in one place

---
+++ LET_Auftragsverwaltung/CS_Parameter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LET_Auftragsverwaltung
+{
+    static class Parameter
+    {
+        public static string Date_Format
+        {
+            get
+            {
+                return "ddd dd/MM/yyyy";
+            }
+        }
+    }
+}
